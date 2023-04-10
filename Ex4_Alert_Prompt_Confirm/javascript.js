9c6fae08d09e9b41376126c9267176917a237e7d
tedisucdemorcov <110901389+tedisucdemorcov@users.noreply.github.com>
2023-04-10 21:06:48 +0300

    Add files via upload

---
+++ Ex4_Alert_Prompt_Confirm/javascript.js
@@ -0,0 +1,9 @@
+let inceput = ("Acesta este inceputul exercitiului.");
+let go = ("sa incepem exercitiul");
+prompt (inceput,);
+let intrebare = confirm ( "esti pregatit de exercitiu?");
+alert (intrebare);
+let intrebare_neg = ( "nu conta oricum =)");
+alert (intrebare_neg);
+let name = confirm("Are you John?");
+alert(name);

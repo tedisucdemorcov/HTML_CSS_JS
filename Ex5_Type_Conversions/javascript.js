47d039d66b9dd667644712d5a0e6254d84d5f531
tedisucdemorcov <110901389+tedisucdemorcov@users.noreply.github.com>
2023-04-11 14:26:27 +0300

    Add files via upload

---
+++ Ex5_Type_Conversions/javascript.js
@@ -0,0 +1,13 @@
+
+
+let value = true;
+alert(typeof value); // boolean
+
+value = String(value); // now value is a string "true"
+alert(typeof value); // string
+
+let value1 = Boolean (true);
+alert(value1);
+
+value1 = Number(value1);
+alert(value1);

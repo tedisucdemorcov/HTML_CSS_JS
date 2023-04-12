73452908e5852542b252289f13eb09a834dc2052
tedisucdemorcov <110901389+tedisucdemorcov@users.noreply.github.com>
2023-04-12 20:43:51 +0300

    Add files via upload

---
+++ Ex6_Math/javascript.js
@@ -0,0 +1,14 @@
+
+// What are the final values of all variables a, b, c and d after the code below?
+
+let a = 1, b = 1;
+
+let c = ++a; // ?
+let d = b++; // ?
+
+alert ( "a = 1 si b = 1 " + " " + "c = ++a ?"+ " d = b++ ?" + " " + " este egal cu = "+ " " +" c= 2 si d= 1" );
+alert ( c);
+alert ( d);
+
+let explicatie = String ("explicatia e simpla, ++a inseamna ca la ca la a se mai adauga un 1 deci 1+1 in timp ce la b++ ii crestem identatia dar nu avem ce");
+alert (explicatie);
